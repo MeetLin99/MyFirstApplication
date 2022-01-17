5da8e6b6f0873152e4495b6062d20f1842ca1155
lin <239940539@qq.com>
2022-01-17 11:24:31 +0800

    20220117a

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -16,24 +16,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startTestOne(View view) {
-        String testerName = ((EditText) findViewById(R.id.userNameText)).getText().toString();
-        String testerPhoneAge = ((EditText) findViewById(R.id.phoneUseTimeText)).getText().toString();
-        String testerProfession = ((EditText) findViewById(R.id.userProfession)).getText().toString();
-        int testerAge = 0;
-
-        try {
-            testerAge = Integer.parseInt(((EditText) findViewById(R.id.userAgeText)).getText().toString());
-        }catch (NumberFormatException e) {}
-
-        if(!check(testerName, testerAge, testerPhoneAge, testerProfession)) {
-            Toast.makeText(this, "输入的信息有误, 请重新输入!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        ResDump.testerInfoInit(testerName, testerAge, testerPhoneAge, testerProfession);
+//        String testerName = ((EditText) findViewById(R.id.userNameText)).getText().toString();
+//        String testerPhoneAge = ((EditText) findViewById(R.id.phoneUseTimeText)).getText().toString();
+//        String testerProfession = ((EditText) findViewById(R.id.userProfession)).getText().toString();
+//        int testerAge = 0;
+//
+//        try {
+//            testerAge = Integer.parseInt(((EditText) findViewById(R.id.userAgeText)).getText().toString());
+//        }catch (NumberFormatException e) {}
+//
+//        if(!check(testerName, testerAge, testerPhoneAge, testerProfession)) {
+//            Toast.makeText(this, "输入的信息有误, 请重新输入!", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+//        ResDump.testerInfoInit(testerName, testerAge, testerPhoneAge, testerProfession);
         Intent intent = new Intent(this, TestOneActivity.class);
         startActivity(intent);
     }
+
+    public void startTestOne2(View view) {
+//        String testerName = ((EditText) findViewById(R.id.userNameText)).getText().toString();
+//        String testerPhoneAge = ((EditText) findViewById(R.id.phoneUseTimeText)).getText().toString();
+//        String testerProfession = ((EditText) findViewById(R.id.userProfession)).getText().toString();
+//        int testerAge = 0;
+//
+//        try {
+//            testerAge = Integer.parseInt(((EditText) findViewById(R.id.userAgeText)).getText().toString());
+//        }catch (NumberFormatException e) {}
+//
+//        if(!check(testerName, testerAge, testerPhoneAge, testerProfession)) {
+//            Toast.makeText(this, "输入的信息有误, 请重新输入!", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+//        ResDump.testerInfoInit(testerName, testerAge, testerPhoneAge, testerProfession);
+        Intent intent2 = new Intent(this, TestOneActivity2.class);
+        startActivity(intent2);
+    }
+
+    public void startTestOne3(View view) {
+//        String testerName = ((EditText) findViewById(R.id.userNameText)).getText().toString();
+//        String testerPhoneAge = ((EditText) findViewById(R.id.phoneUseTimeText)).getText().toString();
+//        String testerProfession = ((EditText) findViewById(R.id.userProfession)).getText().toString();
+//        int testerAge = 0;
+//
+//        try {
+//            testerAge = Integer.parseInt(((EditText) findViewById(R.id.userAgeText)).getText().toString());
+//        }catch (NumberFormatException e) {}
+//
+//        if(!check(testerName, testerAge, testerPhoneAge, testerProfession)) {
+//            Toast.makeText(this, "输入的信息有误, 请重新输入!", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+//        ResDump.testerInfoInit(testerName, testerAge, testerPhoneAge, testerProfession);
+        Intent intent3 = new Intent(this, TestOneActivity3.class);
+        startActivity(intent3);
+    }
+
+
+
+
+
+
+
 
     private boolean check(String name, int age, String phoneAge, String profession) {
         return age > 0 && name.length() > 0 && phoneAge.length() > 0 && profession.length() > 0;
